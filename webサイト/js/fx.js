cc699848efe085c96cc1371e23e05ca6b472cfb2
Felipe Felix de Luca <felipefelixluca@gmail.com>
2016-09-22 08:30:07 +0900

    deleting files

--- webサイト/js/fx.js
+++ webサイト/js/fx.js
@@ -1,4 +1,36 @@
-/*jslint bitwise: true */
+﻿//shuffle
+
+jQuery.fn.extend({
+
+	shuffleEffect: function(duration) {
+
+		if(duration == null) duration = 50;
+		var arrLetter = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9"];
+		var $this = jQuery(this);
+		var strDefault = $this.text();
+		var nLength = strDefault.length - 1;
+		var i = 0;
+		var strText = "";
+		var tid = setInterval(function() {
+			if(i < nLength+1){
+				var strShuffle = "";
+				for(var j=0; j<nLength-i; j++){
+					strShuffle += arrLetter[Math.floor(Math.random() * arrLetter.length)];
+				}
+				strText += strDefault.charAt(i);
+				$this.css({display:"block"}).text(strText + strShuffle);
+				i++;
+			} else {
+				clearInterval(tid);
+			}
+		}, duration);
+
+	}
+
+});
+
+//dots generator
+
 (function ($) {
     "use strict";
     $.fn.generateBackground = function (options) {
@@ -37,3 +69,20 @@
 }(jQuery));
 
 $('#play').generateBackground();
+
+//scroll
+
+$(function() {
+		  $('a[href*="#"]:not([href="#"])').click(function() {
+		    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+		      var target = $(this.hash);
+		      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		      if (target.length) {
+		        $('html, body').animate({
+		          scrollTop: target.offset().top
+		        }, 1000);
+		        return false;
+		      }
+		    }
+		  });
+		});
